37d1dd53488d70585db2665ba4a9509c7c9915ac
e <popijun@163.com>
2019-03-03 10:51:47 +0800

    依赖管理系统

---
+++ hpetshop-parent/.idea/fileTemplates/includes/File Header.java
@@ -0,0 +1,10 @@
+/**     
+  * 
+  * @ProjectName:    ${PROJECT_NAME}
+  * @Package:        ${PACKAGE_NAME}
+  * @Author:         wushaochuan
+  * @CreateDate:     ${DATE} ${TIME}
+  * @UpdateUser:     
+  * @UpdateDate:     ${DATE} ${TIME}
+  * @Version:        1.0
+  **/
